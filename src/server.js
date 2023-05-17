a04f80fe93c35facee10e414b7a9b0077a95b6cf
George Paterson <paterson.george@gmail.com>
2023-05-17 19:44:06 +0100

    Update to resolve the CORS issue triggered by new Blob.

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const app = express();
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({
-  extended: false
-})
-);
-app.use(bodyParser.json());
+app.use(bodyParser.text({ type: '*/*' }));
+app.use(cors());
+app.options('*', cors());
 
-app.post("/api/beacon", (req, res) => {
-  console.log(req.body);
+app.post('/api/beacon', (req, res) => {
+  const data = req.body;
+  // Work with analytics data as a JavaScript object
+  console.log(data);
   console.log(`ip: ${req.ip} url: ${req.hostname +''+ req.url} received: ${new Date().toISOString()}`);
-  return res.status(204);
+  res.status(200).send(true);
 });
 
 app.use(express.static(path.resolve(__dirname, "../public")));
 
-const PORT = 9000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
